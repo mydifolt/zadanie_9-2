3e63838174469486075b5b383a1c69a2913311d0
Piotrek <mydifolt@gmail.com>
2017-05-18 18:55:49 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+var allNames = femaleNames.concat(maleNames);
+
+var newName = 'Marian';
+
+if (allNames.indexOf(newName) === -1){
+	var allNames2 = allNames.push('Marian');
+}
+
+
+
+
+
